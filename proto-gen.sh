72999a18bee9ef0784101707a1eb80564ca46e3d
DoniyorAli <doniyorfarhodov96@gmail.com>
2023-01-03 22:34:20 +0500

    add proto-gen.sh

--- proto-gen.sh
+++ proto-gen.sh
@@ -1,7 +1,3 @@
 protoc --go_out=./protogen \
     --go-grpc_out=./protogen \
-    ./protoc/brand/*.proto
-
-protoc --go_out=./protogen \
-    --go-grpc_out=./protogen \
-    ./protoc/car/*.proto
+    ./rentcar_protoc/*.proto
